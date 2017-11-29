48ec499e10fdcb5a85e3c090cbc85b4ded575b02
Sayem Khan <sayemmkhan@gmail.com>
2017-11-28 22:47:58 -0500

    Initial commit

---
+++ ajax-email.php
@@ -0,0 +1,33 @@
+<?php
+
+/* SETTINGS */
+$yourEmail = "contact@copadigitalgroup.com";
+$emailSubject = "Copa";
+
+if( $_POST ) {
+
+  /* DATA FROM HTML FORM */
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $emailSubject = $emailSubject . " by " . $name;
+
+  //Set from address to domain name. If set to example (gmail or yahoomail), it will get rejected because of the DMAC policy.
+  $headers = "From: $name <noreply@copadigital.com>\r\n" .
+             "Reply-To: $name <$email>\r\n" . 
+             "Subject: $emailSubject\r\n" .
+             "Content-type: text/plain; charset=UTF-8\r\n" .
+             "MIME-Version: 1.0\r\n" . 
+             "X-Mailer: PHP/" . phpversion() . "\r\n";
+ 
+  /* PREVENT EMAIL INJECTION */
+  if ( preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $email)) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die("500 Internal Server Error");
+  }
+
+  /* SEND EMAIL */
+  mail($yourEmail, $emailSubject, $message, $headers);
+}
+
+?>
