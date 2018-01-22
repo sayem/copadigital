6e76bc007f8430594979a1ca437ec03223464e09
Sayem Khan <sayemmkhan@gmail.com>
2018-01-22 15:02:55 -0500

    Updated site

--- ajax-email.php
+++ ajax-email.php
@@ -1,7 +1,7 @@
 <?php
 
-/* SETTINGS */
-$yourEmail = "hello@copadigitalgroup.com";
+ini_set("sendmail_from", "noreply@copadigitalgroup.com");
+$yourEmail = "team@copadigitalgroup.com";
 $emailSubject = "Copa Digital - Inquiry";
 
 if( $_POST ) {
@@ -16,13 +16,16 @@ if( $_POST ) {
   $emailSubject = $emailSubject . " by " . $name;
 
   //Set from address to domain name. If set to example (gmail or yahoomail), it will get rejected because of the DMAC policy.
-  $headers = "From: $name <noreply@copadigital.com>\r\n" .
+  $headers = "From: $name <noreply@copadigitalgroup.com>\r\n" .
              "Reply-To: $name <$email>\r\n" . 
+             //"Return-Path: <noreply@copadigitalgroup.com>\r\n" . // this is not working, but mail(..., -f email@domain) is working
              // "Company: $company\r\n" .
              // "Phone Number: $phone\r\n" .
              "Subject: $emailSubject\r\n" .             
-             "Content-type: text/plain; charset=UTF-8\r\n" .
+             //"Content-type: text/plain; charset=UTF-8\r\n" .
+             'Content-Type: text/plain; charset="UTF-8"' . "\r\n";
              "MIME-Version: 1.0\r\n" . 
+             "Content-Disposition: inline\r\n" . 
              "X-Mailer: PHP/" . phpversion() . "\r\n";
  
   /* PREVENT EMAIL INJECTION */
@@ -31,13 +34,13 @@ if( $_POST ) {
     die("500 Internal Server Error");
   }
 
-  $message = 'Name: '. $name . "\r\n";
+  $message .= 'Name: '. $name . "\r\n";
   $message .= 'Company: '. $company . "\r\n";
   $message .= 'Phone: '. $phone . "\r\n";
   $message .= "Message: \r\n". $msg;
 
   /* SEND EMAIL */
-  mail($yourEmail, $emailSubject, $message, $headers);
+  mail($yourEmail, $emailSubject, $message, $headers, "-f noreply@copadigitalgroup.com");
 }
 
 ?>
